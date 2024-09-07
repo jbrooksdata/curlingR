ef40332924fa66e15a72c976cb6f4cac267749ff
Jeremy Brooks <jeremydanielbrooks@yahoo.com>
2024-09-07 01:24:45 -0500

    fix bind_rows

--- R/load_events_games.R
+++ R/load_events_games.R
@@ -1,9 +1,10 @@
 load_events_games <- function(event_id) {
+  library(dplyr)
     file <- bind_rows(read.csv(
       paste0("https://raw.githubusercontent.com/jbrooksdata/curlingR-data/main/mensEventsGames.csv"),
       header = TRUE), read.csv(
         paste0("https://raw.githubusercontent.com/jbrooksdata/curlingR-data/main/womensEventsGames.csv"),
-        header = TRUE)) 
+        header = TRUE))
     file <- file[, -1]
     file <- file[file$event_id == event_id,]
     rownames(file) <- NULL
--- R/load_events_stats.R
+++ R/load_events_stats.R
@@ -1,9 +1,10 @@
 load_events_stats <- function(event_id) {
+  library(dplyr)
     file <- bind_rows(read.csv(
       paste0("https://raw.githubusercontent.com/jbrooksdata/curlingR-data/main/mensEventsStats.csv"),
       header = TRUE), read.csv(
         paste0("https://raw.githubusercontent.com/jbrooksdata/curlingR-data/main/womensEventsStats.csv"),
-        header = TRUE)) 
+        header = TRUE))
     file <- file[, -1]
     file <- file[file$event_id == event_id,]
     rownames(file) <- NULL
